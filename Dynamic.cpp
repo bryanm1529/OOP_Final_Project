2f4d8bd12a23d203e79d38025023625fe2932486
Bryan Martinez <bmartin7@ramapo.edu>
2022-05-08 18:15:07 -0400

    Trying to initalized a list of functions (remove func and add func) to our constructor

--- Dynamic.cpp
+++ Dynamic.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "Dynamic.h"
+
+list<function<void()>> functions;
+
+Dynamic::Dynamic(){
+    functions.push_back(remove_func);
+    functions.push_back(add_func);
+};
+
+void remove_func(f){};
+void add_func(f){};
--- Dynamic.h
+++ Dynamic.h
@@ -4,10 +4,15 @@
 
 #ifndef OOP_FINAL_PROJECT_DYNAMIC_H
 #define OOP_FINAL_PROJECT_DYNAMIC_H
-
-
+#include <list>
+using namespace std;
 class Dynamic {
+    list<function<void()>> functions;
 
+    Dynamic();
+
+    void remove_func(f);
+    void add_func(f);
 };
 
 
