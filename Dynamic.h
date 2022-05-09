a1ef380eec89f88683648cc0b02185f15ad27507
Bryan Martinez <bmartin7@ramapo.edu>
2022-05-08 22:39:13 -0400

    Created constructor to store add our function objects into our list

--- Dynamic.h
+++ Dynamic.h
@@ -13,13 +13,17 @@ class Dynamic {
 
 public:
     //List to store our functions
-    list<function<void()>>functions;
+    list<function<void(function<void()>)>>functions;
 
     //Here we store a call to a function object where we will then store these function objects into our list
     function<void(function<void()>)> remove_func_object = remove_func();
     function<void(function<void()>)> add_func_object = add_func();
 
-
+    //Constructor will insert our 2 main function objects into our list
+    Dynamic(){
+        functions.push_back(remove_func_object);
+        functions.push_back(add_func_object);
+    };
 
     //These are our functions which we need to overload operator() for them to work
     struct remove_func {
