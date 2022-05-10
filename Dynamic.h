0ca68684bd47588bec20cb4e363c91a962623d98
Bryan Martinez <bmartin7@ramapo.edu>
2022-05-10 18:04:30 -0400

    Changed everything to one file and am not trying to make a driver program to test our class and its functionality

--- Dynamic.h
+++ Dynamic.h
@@ -4,41 +4,7 @@
 
 #ifndef OOP_FINAL_PROJECT_DYNAMIC_H
 #define OOP_FINAL_PROJECT_DYNAMIC_H
-#include <list>
-#include <iostream>
-using namespace std;
 
-
-class Dynamic {
-
-public:
-    //List to store our functions
-    list<function<void()>>functions;
-
-    //Constructor to initialize our variables and copy the initial list of functions
-    Dynamic(const list<function<void()>>& initialList){
-        //Copying the initial list to our functions list
-        functions = initialList;
-
-    };
-
-    //Here we store a call to a function object to the remove and add functions this allows us to access the function at runtime
-    function<void(function<void()>)> remove_func_object = remove_func();
-    function<void(function<void()>)> add_func_object = add_func();
-
-    //These are our functions which we need to overload operator() for them to work
-    struct remove_func {
-        void operator()(function<void()> f) const {
-            //Code to remove function from class
-        }
-    };
-
-    struct add_func {
-        void operator()(function<void()> f) const {
-            //Code to add function from class
-        }
-    };
-};
 
 
 #endif //OOP_FINAL_PROJECT_DYNAMIC_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
-#include "Dynamic.h"
+#include <list>
+
 using namespace std;
 
+
+class Dynamic {
+
+public:
+    //List to store our functions
+    static list<void*>functions;
+
+    //Constructor to initialize our variables and copy the initial list of functions
+    Dynamic(const list<void*>& initialList){
+        //Copying the initial list to our functions list
+        functions = initialList;
+
+    };
+
+    Dynamic(){};
+
+    //Here we store a call to a function object to the remove and add functions this allows us to access the function at runtime
+    function<void(void*)> remove_func_object = remove_func();
+    function<void(void*)> add_func_object = add_func();
+
+    //These are our functions which we need to overload operator() for them to work
+    struct remove_func {
+        void operator()(void *f) const {
+            //Code to remove function from class
+            functions.remove(f);
+        }
+    };
+
+    struct add_func {
+        void operator()(void* f) const {
+            //Code to add function to class list
+            functions.push_back(f);
+        }
+    };
+};
+
+//test function to add or delete
+void* dog(){
+    cout << " hi" << endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Dynamic test;
+
+
     return 0;
 }
